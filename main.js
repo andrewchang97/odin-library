13e47216a2387b8c5f0118353162a11cf1b11a3c
Andrew <andrewhoonchang97@gmail.com>
2023-08-23 14:23:24 -0700

    Adding updates to javascript and functionality, should be working, but not storing locally

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ function Book(name, author, numberOfPages, finished) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
-
-  console.log("success")
+  newBook = new Book(name, author, numberOfPages, finished);
+  myLibrary.push(newBook)
+  form.reset();
 }
 let newBook = document.querySelector(".createNew");
 newBook.addEventListener("click", addBookToLibrary);
@@ -31,3 +31,35 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
   }
+  document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("insideModal");
+    const dataDisplay = document.getElementById("libraryID");
+
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        
+        const title = document.getElementById("book_title").value;
+        const author = document.getElementById("author").value;
+        const pageCount = document.getElementById("page_count").value;
+        const finished = document.getElementById("finished").checked ? "Finished" : "Not Finished";
+
+        // Create a new entry container
+        const newEntry = document.createElement("div");
+        newEntry.classList.add("entry"); // Add a class for styling if needed
+        newEntry.innerHTML = `
+            <p><strong>Title:</strong> ${title}</p>
+            <p><strong>Author:</strong> ${author}</p>
+            <p><strong>Page Count:</strong> ${pageCount}</p>
+            <p><strong>Status:</strong> ${finished}</p>
+        `;
+
+        // Append the new entry to the data display area
+        dataDisplay.appendChild(newEntry);
+
+        // Clear the form fields
+        document.getElementById("book_title").value = "";
+        document.getElementById("author").value = "";
+        document.getElementById("page_count").value = "";
+        document.getElementById("finished").checked = true;
+    });
+});
